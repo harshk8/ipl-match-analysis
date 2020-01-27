aec7f7e95772130f50750ac5d81e0a848ad051a5
harshk8 <harshkhatke8@gmail.com>
2020-01-28 00:10:09 +0530

    temp change

--- ipl_analysis/settings.py
+++ ipl_analysis/settings.py
@@ -125,8 +125,8 @@ SITE_ID = 1
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
